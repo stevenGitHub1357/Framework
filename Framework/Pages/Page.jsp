dee609aafa3f6ab82a49157fef3bfdda3f5f7631
stevenGitHub1357 <stevenratovoson@gmail.com>
2023-07-07 22:11:54 +0300

    SPRINT6

--- Framework/Pages/Page.jsp
+++ Framework/Pages/Page.jsp
@@ -1,12 +1,40 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<%@page import="model.Emp" %>
+
+<%
+    Emp[] list = null;
+
+    if(request.getAttribute("lst") != null) {
+        list = (Emp[]) request.getAttribute("lst");
+    }
+%>
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Url marche
-</body>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Welcome this page</h1>
+
+        <table border=1>
+            <tr>
+                <th> IdEmp </th>
+                <th> Nom </th>
+                <th> Prenom </th>
+                <th> Age </th>
+            </tr>
+            <% for(int i = 0; i < list.length; i++) { %>
+            <tr>
+                <td> <%=list[i].getIdEmp()%> </td>
+                <td> <%=list[i].getNom()%> </td>
+                <td> <%=list[i].getPrenom()%> </td>
+                <td> <%=list[i].getAge()%> </td>
+                <td> <a href="emp-findById?id=<%=list[i].getIdEmp() - 1%>">Detail</a> </td>
+            </tr>
+            <% } %>
+        </table>
+    </body>
 </html>
--- Framework/WEB-INF/src/Emp.java
+++ Framework/WEB-INF/src/Emp.java
@@ -1,15 +1,80 @@
-package modele;
-import modele.ModelView;
-// package etu1889.framework;
-import etu1889.framework.annotation.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
 
-public class Emp{
-    ModelView modele = new ModelView("Page.jsp");
-    public Emp(){
+import etu1889.framework.annotation.AnnotationUrl;
+import etu1889.framework.ModelView;
 
+public class Emp {
+    int idEmploye;
+    String nom;
+    String prenom;
+    int age;
+
+    public Emp() {
     }
-    @AnnotationUrl(url="emp-add")
-    public ModelView add(){
-        return modele;
+
+    public Emp(int idEmploye, String nom, String prenom, int age) {
+        this.idEmploye = idEmploye;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    public Emp[] listEmp() {
+        Emp[] ans = new Emp[2];
+        ans[0] = new Emp(1, "RAKOTO", "Nirina", 20);
+        ans[1] = new Emp(2, "RAHARY", "Soa", 19);
+
+        return ans;
+    }
+    
+    @AnnotationUrl(url = "emp-all")
+    public ModelView findAll() {
+        ModelView view = new ModelView("page.jsp");
+        view.addItem("lst", this.listEmp());
+        view.addItem("test", 12);
+
+        System.out.println("etu1889.framework.Employe.findAll()");
+        
+        return view;
+    }
+
+    @AnnotationUrl(url = "emp-view")
+    public ModelView findView() {
+        ModelView view = new ModelView("view.html");
+        
+        return view;
+    }
+
+    public int getIdEmp() {
+        return this.idEmploye;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String snom) {
+        this.nom = snom;
+    }
+
+    public String getPrenom() {
+        return this.prenom;
+    }
+
+    public void setPrenom(String sprenom) {
+        this.prenom = sprenom;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int sage) {
+        this.age = sage;
     }
 }
